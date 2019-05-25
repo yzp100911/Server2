547a426e84b4ac2f5248d54b9e9be7f35e6c0b31
yzp100911 <1178375453@qq.com>
2019-05-25 15:10:52 +0800

    初次提交

---
+++ broker/src/main/java/com/xiaoleilu/loServer/action/VersionAction.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the Wildfire Chat package.
+ * (c) Heavyrain2012 <heavyrain.lee@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+package com.xiaoleilu.loServer.action;
+
+import com.google.gson.Gson;
+import com.xiaoleilu.loServer.annotation.HttpMethod;
+import com.xiaoleilu.loServer.annotation.Route;
+import com.xiaoleilu.loServer.handler.Request;
+import com.xiaoleilu.loServer.handler.Response;
+import cn.wildfirechat.pojos.InputUserLogin;
+import cn.wildfirechat.pojos.OutputLoginData;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import win.liyufan.im.GitRepositoryState;
+
+import java.io.IOException;
+
+@Route("/api/version")
+@HttpMethod("GET")
+public class VersionAction extends Action {
+
+
+    @Override
+    public boolean action(Request request, Response response) {
+        if (request.getNettyRequest() instanceof FullHttpRequest) {
+            response.setStatus(HttpResponseStatus.OK);
+
+            try {
+                response.setContent(new Gson().toJson(GitRepositoryState.getGitRepositoryState()));
+            } catch (IOException e) {
+                e.printStackTrace();
+                response.setContent("{\"version\":\"unknown\"}");
+            }
+        }
+        return true;
+    }
+}
---
+++ client/src/main/java/com/xiaoleilu/loServer/pojos/InputRoute.java
@@ -0,0 +1,32 @@
+package cn.wildfirechat.pojos;
+
+
+public class InputRoute {
+    private String userId;
+    private String token;
+    private String clientId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+}
